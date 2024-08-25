6992af6233346d8ca382be1d71c4928069b0e3e2
iwanttobealight <iwanttobealight@yandex.ru>
2024-08-25 20:55:21 +0200

    inital commit

---
+++ browser.py
@@ -0,0 +1,22 @@
+from url import URL
+
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+def load(url):
+    body = url.request()
+    print('elele', body)
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
